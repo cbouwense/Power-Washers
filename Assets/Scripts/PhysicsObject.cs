1a76992df6f52f351b83ae23ef766869f3c2c83c
Christian <bouwensec@gmail.com>
2017-09-22 20:37:48 -0400

    Initial commit

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    public float minGroundNormalY = 0.65f;
+    public float gravityModifier = 5f;
+
+    public bool grounded;
+    protected Vector2 groundNormal;
+    public float velocityX;
+    protected Rigidbody2D rb2d;
+    public Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    protected virtual void Start()
+    {
+        // Ignore any contacts involving trigger colliders
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+
+        // TODO: Pretty sure we don't need this
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
+    {
+        ComputeVelocity();
+    }
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = velocityX;
+
+        grounded = false;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+        Vector2 move;
+
+        move = moveAlongGround * deltaPosition.x;
+        Movement(move, 'x');
+
+        move = Vector2.up * deltaPosition.y;
+        Movement(move, 'y');
+    }
+
+    protected void Movement(Vector2 move, char axis)
+    {
+
+        // Distance that object is going to move
+        float distance = move.magnitude;
+
+        // Only check for collision if we are trying to move 
+        if (distance > minMoveDistance)
+        {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                // Vector perpendicular to the RayCast which detected collision and with what it collided
+                Vector2 currentNormal = hitBufferList[i].normal;
+
+                // If what we collided with is flat enough to be considered ground
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    // Set grounded equal to true
+                    grounded = true;
+                    // If we are modifying the object's y coordinate
+                    if (axis == 'y')
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity -= projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                if (modifiedDistance < distance)
+                {
+                    distance = modifiedDistance;
+                }
+            }
+        }
+
+        rb2d.position += move.normalized * distance;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    
+    protected override void Update()
+    {
+       
+    }
+
+    protected override void ComputeVelocity()
+    {
+
+        Vector2 move = Vector2.zero;
+
+        if (cs.hitstunLeft <= 0 && inv.useStunLeft <= 0 || (inv.useStunLeft > 0 && !grounded))
+        {
+            if (!grounded || !ma.attacking)
+            {
+                move.x = Input.GetAxisRaw("Horizontal");
+                if (grounded && move.x != 0 && walkSoundTimer <= 0)
+                {
+                    walkSoundTimer = 0.35f;
+                    SoundManager.PlaySound("mackWalkSound");
+                }
+            }
+
+            // Crouching
+            if (Input.GetAxisRaw("Vertical") == -1 && (grounded || crouching))
+            {
+                crouching = true;
+                cs.currentSpeed = cs.crouchSpeed;
+            }
+            else
+            {
+                // Code so Mack can't stop crouching if something is above his head
+                GameObject ch = transform.GetChild(0).gameObject;
+                Vector2[] origins = new Vector2[5];
+                RaycastHit2D[] hits = new RaycastHit2D[5];
+                float xBaseOffset = -0.65f;
+                float checkDistance = 0.409f;
+                for (int i = 0; i < 5; i++)
+                {
+                    origins[i] = new Vector2(ch.transform.position.x + xBaseOffset, ch.transform.position.y + 1.19f);
+                    xBaseOffset += 0.325f;
+                    hits[i] = Physics2D.Raycast(origins[i], Vector2.up, checkDistance);
+                    if (hits[i].collider != null)
+                    {
+                        break;
+                    }
+                    else if (i == 4)
+                    {
+                        crouching = false;
+                        cs.currentSpeed = cs.maxSpeed;
+                    }
+                }
+            }
+
+            // Jumping
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                SoundManager.PlaySound("mackJumpSound");
+                velocityY = cs.jumpTakeOffSpeed;
+            }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (velocityY > 0)
+                {
+                    velocityY = velocity.y * 0.5f;
+                }
+            }
+        }
+        velocityX = move.x * cs.currentSpeed;
+    }
+
+    public bool isGrounded()
+    {
+        return grounded;
+    }
+
+}
